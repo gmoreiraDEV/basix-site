f167fe8234a4e1aa22d0a1da053ab9a8a6462994
Guilherme Moreira <guilhermemoreira.web@gmail.com>
2025-12-16 23:53:45 -0300

    Atualiza dependências e aprimora o processo de envio de formulários com melhorias na configuração do banco de dados e notificações por e-mail.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,34 @@
-import type React from "react"
-import type { Metadata } from "next"
-import Script from "next/script"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import Script from "next/script";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
+
+import "./globals.css";
 
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist",
-})
+});
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-geist-mono",
-})
+});
 
 export const metadata: Metadata = {
   title: "Basix Digital - IA sem complicação para profissionais e empresas",
   description:
     "Consultoria e Mentoria em Inteligência Artificial para PMEs e profissionais. Automatize processos, aumente vendas e ganhe produtividade com IA sem complicação.",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" suppressHydrationWarning={true} data-lt-installed="true">
       <head>
         <link rel="icon" type="image/png" href="/assets/img/bx.png" />
         <Script
@@ -44,7 +46,8 @@ export default function RootLayout({
       </head>
       <body className={`${geist.className} ${geistMono.className}`}>
         {children}
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
